22b1b749c5d4f86564c84224c7c4689bf0f383c7
caleb <caleb.easterly@gmail.com>
2018-12-01 22:56:40 -0600

    sort tabular files

--- oral_microbiome_case_study/summaries/make_function_table_supplement.R
+++ oral_microbiome_case_study/summaries/make_function_table_supplement.R
@@ -4,7 +4,7 @@ library(dplyr)
 f <- read.delim('mqome_outputs/func_full_test_out.tab', stringsAsFactors = FALSE) %>%
 	mutate(log2fc_WS_over_NS = (-1) * log2fc_NS_over_WS) %>%
 	filter(corrected_p <= 0.05) %>%
-	arrange(log2fc_WS_over_NS) %>%
+	arrange(-log2fc_WS_over_NS) %>%
 	select(id, name, namespace, log2fc_WS_over_NS, corrected_p)
 
 write.table(f, file="summaries/differentially_expressed_go_terms.tab",
--- oral_microbiome_case_study/summaries/make_taxonomy_table_supplement.R
+++ oral_microbiome_case_study/summaries/make_taxonomy_table_supplement.R
@@ -5,7 +5,7 @@ library(dplyr)
 t <- read.delim('mqome_outputs/tax_test_out.tab', stringsAsFactors = FALSE) %>%
 	mutate(log2fc_WS_over_NS = (-1) * log2fc_NS_over_WS) %>%
 	filter(corrected_p <= 0.05) %>%
-	arrange(log2fc_WS_over_NS) %>%
+	arrange(-log2fc_WS_over_NS) %>%
 	select(taxon_name, log2fc_WS_over_NS, corrected_p)
 
 write.table(t, file="summaries/differentially_expressed_taxa.tab",
