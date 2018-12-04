1ed13e0ec5205ac175cec35afff0cefad6e8d00e
caleb <caleb.easterly@gmail.com>
2018-12-03 22:14:42 -0600

    add ranks to DE tax table

--- oral_microbiome_case_study/summaries/make_taxonomy_table_supplement.R
+++ oral_microbiome_case_study/summaries/make_taxonomy_table_supplement.R
@@ -6,7 +6,7 @@ t <- read.delim('mqome_outputs/tax_test_out.tab', stringsAsFactors = FALSE) %>%
 	mutate(log2fc_WS_over_NS = (-1) * log2fc_NS_over_WS) %>%
 	filter(corrected_p <= 0.05) %>%
 	arrange(-log2fc_WS_over_NS) %>%
-	select(taxon_name, log2fc_WS_over_NS, corrected_p)
+	select(taxon_name, rank, log2fc_WS_over_NS, corrected_p)
 
 write.table(t, file="summaries/supporting3_differentially_expressed_taxa.tab",
 			sep="\t", row.names=FALSE, quote=FALSE)
